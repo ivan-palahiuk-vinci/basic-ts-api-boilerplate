2aabd9736fb79d450e1fd99f441fe7d3b82d323a
ivan-palahiuk-vinci <ivan.palahiuk@student.vinci.be>
2025-09-23 11:18:51 +0200

    new: ex1.2

--- app.ts
+++ app.ts
@@ -9,6 +9,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/users", usersRouter);
-app.use("/pizzas", pizzaRouter);
+app.use((req, res, next)=>{
+    let compteur: number = 0;
+    if(req.method ==='GET'){
+        compteur++;
+        console.log("GET: " + compteur)
+    }
+})
 
 export default app;
